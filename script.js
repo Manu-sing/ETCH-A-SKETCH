ad2c5f98eb19645aae252722948e37939ae9fbe0
Emanuele Guarnaccia <emanueleguarnaccia@gmail.com>
2021-11-16 10:50:23 +0800

    added a function that creates the grid, instead of using css and started working on the hover effect

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
-const container = document.querySelector(".container");
-
-let divsCount = 256;
-let squareDiv;
+const container = document.querySelector("#container");
 
 
+function makeGrid(rows, cols) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-cols', cols);
+    for (i = 0; i < (rows * cols); i++) {
+        let square = document.createElement("div");
+        container.appendChild(square).className = "div-item";
+    };
 
+    const newDivs = document.querySelectorAll('.div-item');
 
-for (let i = 0; i < divsCount; i++) {
-    squareDiv = document.createElement('div');
-    squareDiv.setAttribute('class', 'squareitem');
-    container.appendChild(squareDiv);
-
-}
-
-/*const newDivs = document.querySelectorAll('.squareitem');
-
-newDivs.forEach((div) => {
+    newDivs.forEach((div) => {
     
     div.addEventListener('mouseover', () => {
         div.classList.add('hovercolor')
     })
-})*/
+})
+
+};
+
+makeGrid (16, 16);
+
+
 
 
 
