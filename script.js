9a380d787975bdc0d9ffcd8f9c74bd82b6f5d35d
Emanuele Guarnaccia <emanueleguarnaccia@gmail.com>
2021-11-17 14:11:54 +0800

    added button event listener that asks the user to create a new sketchpad

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const container = document.querySelector("#container");
+let container = document.querySelector("#container");
+const bod = document.querySelector('body');
 
 
 function makeGrid(rows, cols) {
+    
     container.style.setProperty('--grid-rows', rows);
     container.style.setProperty('--grid-cols', cols);
     for (i = 0; i < (rows * cols); i++) {
@@ -20,7 +22,66 @@ function makeGrid(rows, cols) {
 
 };
 
-makeGrid (16, 16);
+makeGrid (50, 50);
+
+
+const button = document.querySelector('button');
+
+button.addEventListener('click', promptMe);
+
+
+function promptMe(){
+    container.remove();
+    let msg = prompt("How many squares per side would you like in your sketchpad?");
+    let userInput = msg;
+    makeNewGrid(userInput, userInput);
+
+    
+}
+
+
+
+function makeNewGrid(userInput, userInput) {
+
+    container = document.createElement('div');
+    container.setAttribute('id', 'container');
+    bod.appendChild(container);
+
+
+    
+    
+    container.style.setProperty('--grid-rows', userInput);
+    container.style.setProperty('--grid-cols', userInput);
+    for (i = 0; i < (userInput * userInput); i++) {
+        if(userInput > 100) {
+            container.remove()
+            alert('You can only choose a maximum of 100 squares per side. Create again :)');
+            return;
+        }
+        let square = document.createElement("div");
+        container.appendChild(square).className = "div-item";
+    };
+
+    const newDivs = document.querySelectorAll('.div-item');
+
+    newDivs.forEach((div) => {
+    
+    div.addEventListener('mouseover', () => {
+        div.classList.add('hovercolor')
+    })
+})
+
+
+}
+
+
+
+
+
+
+
+
+
 
 
 
